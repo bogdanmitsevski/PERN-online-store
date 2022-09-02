718bb68e7faee4a40ca4e473fafb04eeba302f30
Bohdan Mitsevskyi <mitsevskyi.corporate@gmail.com>
2022-09-02 12:24:53 +0300

    Last commit without npm

---
+++ server/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models');
+const router = require('./routes/index');
+const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const app = express();
+const errorHandlerMiddleWare = require('./middleware/ErrorHandlingMiddleware');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use('/api', router);
+//Error handling, last Middleware
+app.use(errorHandlerMiddleWare);
+
+const start = async ()=> {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, ()=> console.log(` Server started on ${PORT}`));
+    }
+    catch(e) {
+        console.log(e);
+ }
+}
+
+start();
